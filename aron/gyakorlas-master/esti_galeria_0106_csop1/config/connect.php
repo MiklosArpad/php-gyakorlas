f261a5b90c7226dc94d762eda827ecfe06e7f1ed
MiklosArpad <43209274+MiklosArpad@users.noreply.github.com>
2020-10-05 14:03:48 +0200

    1

--- aron/gyakorlas-master/esti_galeria_0106_csop1/config/connect.php
+++ aron/gyakorlas-master/esti_galeria_0106_csop1/config/connect.php
@@ -1,6 +1,12 @@
 <?php
 
-$con = new mysqli('localhost','root','','esti_galeria','3306');
+$host = 'localhost';
+$username = 'root1';
+$password = 'root1';
+$database = 'esti_galeria';
+$port = 3306;
+
+$con = new mysqli($host, $username, $password, $database, $port);
 
 if ($con -> errno){
     die('Nem sikerült csatlakozi az adatbázishoz!');
